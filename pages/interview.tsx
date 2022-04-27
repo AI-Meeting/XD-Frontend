04661bb878b2f5da23dbc899d1c530d8592360e1
강은빈 <kub0803@naver.com>
2022-04-27 15:49:17 +0900

    [FIX] div -> selection 변경

--- pages/interview.tsx
+++ pages/interview.tsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import { NextPage } from "next";
 import Header from "../components/common/header";
+import ControllNavBar from "../components/company/controllNavBar";
 import MyInterview from "../components/myInterview";
 
 const InterviewPage: NextPage<{}> = () => {
@@ -12,7 +13,7 @@ const InterviewPage: NextPage<{}> = () => {
   );
 };
 
-export const Container = styled.div`
+export const Container = styled.section`
   width: 100%;
   height: 100%;
   margin: 0 auto;

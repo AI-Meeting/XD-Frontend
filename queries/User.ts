8f157c3f916611419ba896ad2394cf740e081599
강은빈 <kub0803@naver.com>
2022-05-23 10:13:50 +0900

    [FEAT] queries User add

--- queries/User.ts
+++ queries/User.ts
@@ -1,6 +1,7 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import { UserType } from "../@types/UserType";
 import queryKeys from "../contexts/QueryKey";
+import question from "../lib/api/question";
 import user from "../lib/api/user";
 
 export const useUserInfo = () =>
@@ -11,7 +12,52 @@ export const useUserInfo = () =>
   });
 
 export const useMyInterview = () =>
-  useQuery(queryKeys.myInterview, {
+  useQuery(queryKeys.myInterview, () => user.getMyInterview(), {
     cacheTime: 5,
     staleTime: Infinity,
   });
+
+export const usePostQuestionAnswer = (answer, questionId) => {
+  const client = useQueryClient();
+
+  return useMutation(
+    queryKeys.postQuestionAnswer,
+    () => question.patchQuestionAnswer(answer, questionId),
+    {
+      onSuccess: () => {
+        client.invalidateQueries(queryKeys.company);
+      },
+      onError: () => {},
+    }
+  );
+};
+
+export const useDeleteQuestionAnswer = (answerId) => {
+  const client = useQueryClient();
+
+  return useMutation(
+    queryKeys.deleteQuestionAnswer,
+    () => question.deleteQuestionAnswer(answerId),
+    {
+      onSuccess: () => {
+        client.invalidateQueries(queryKeys.company);
+      },
+      onError: () => {},
+    }
+  );
+};
+
+export const usePatchQuestionAnswer = (answer, answerId) => {
+  const client = useQueryClient();
+
+  return useMutation(
+    queryKeys.patchQuestionAnswer,
+    () => question.patchQuestionAnswer(answer, answerId),
+    {
+      onSuccess: () => {
+        client.invalidateQueries(queryKeys.company);
+      },
+      onError: () => {},
+    }
+  );
+};

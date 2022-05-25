ed1b4e9a39370acda37dada88a8cdcc3814fa798
강은빈 <kub0803@naver.com>
2022-05-25 15:05:00 +0900

    [FEAT] Question queries 추가

--- queries/Question.ts
+++ queries/Question.ts
@@ -2,11 +2,17 @@ import { useMutation, useQuery, useQueryClient } from "react-query";
 import queryKeys from "../contexts/QueryKey";
 import question from "../lib/api/question";
 
-export const useCompany = (companyId: number) =>
-  useQuery(queryKeys.company, () => question.getCompany(companyId), {
-    cacheTime: 5,
-    staleTime: Infinity,
-  });
+export const useCompany = (companyId: number) => {
+  return useQuery(
+    [queryKeys.company, companyId],
+    () => question.getCompany(companyId),
+    {
+      enabled: !!companyId,
+      cacheTime: 5,
+      staleTime: Infinity,
+    }
+  );
+};
 
 export const useDeleteCompany = (companyId: number) => {
   const client = useQueryClient();

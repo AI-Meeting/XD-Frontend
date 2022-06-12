d2aa76827f5391660956fd40688f3a043e375449
Kimjimin0807 <jm004471@naver.com>
2022-06-12 14:55:16 +0900

    [ADD] company review write page

---
+++ components/writeReview/WriteReview.tsx
@@ -0,0 +1,43 @@
+import styled from "@emotion/styled";
+import React, { FC } from "react";
+
+const WriteReview: FC = () => {
+  return (
+    <Container>
+      <Title>면접 후기 등록</Title>
+      <Description>
+        면접 후기 등록을 통해 자신의 면접을 공유 할 수 있어요.
+      </Description>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 600px;
+  height: auto;
+  max-height: calc(100vh - 120px);
+  margin-top: 30px;
+  padding: 45px 80px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  box-sizing: border-box;
+`;
+
+const Title = styled.h1`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const Description = styled.p`
+  width: 100%;
+  padding: 10px;
+  background-color: #f4f9fd;
+  border-radius: 3px;
+  box-sizing: border-box;
+  font-size: 14px;
+  color: #6f828c;
+  margin-top: 10px;
+  margin-bottom: 35px;
+`;
+
+export default WriteReview;
---
+++ components/writeReview/formBox/FormBox.tsx
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+import React, { FC } from "react";
+
+const FormBox: FC = () => {
+  return <Container>
+
+    
+  </Container>;
+};
+
+const Container = styled.div`
+  width: 100%;
+`;
+
+export default FormBox;

27f1ce0d3b0ce6420f9843f5ca699393c80df9a1
강은빈 <kub0803@naver.com>
2022-06-21 22:29:46 +0900

    [FIX] 영상 녹화 api 수정

--- lib/api/question/index.ts
+++ lib/api/question/index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
 import { QuestionAnwerType } from "../../../@types/QuestionType";
-import request from "../common";
+import request, { MAINURL } from "../common";
 
 export default {
   getCompany(companyId: number) {
@@ -15,11 +16,33 @@ export default {
     });
   },
   postQuestionAnswer(answer: QuestionAnwerType, questionId: number) {
-    return request({
-      method: "post",
-      url: `/question/answer?questionId=${questionId}`,
-      data: answer,
-    });
+    const formData: any = new FormData();
+    const uri = answer.videoUrl;
+
+    console.log(answer.videoUrl);
+    const data = {
+      uri,
+    };
+
+    formData.append("videoUrl", data);
+    // formData.append("voiceUrl", answer.voiceUrl);
+    formData.append("answer", answer.answer);
+
+    try {
+      return axios({
+        method: "post",
+        url: `http://localhost:3000/question/answer?questionId=${questionId}`,
+        headers: {
+          // Accept: "*/*",
+          // type: "formData",
+          "Content-Type": "multipart/form-data",
+          Authorization: "Bearer " + localStorage.getItem("access-token"),
+        },
+        data: formData,
+      });
+    } catch (e) {
+      console.log(e);
+    }
   },
   deleteQuestionAnswer(answerId: number) {
     return request({
